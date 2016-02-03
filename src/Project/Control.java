5fc2e6ff1fa73b706e00ccc2d8ca53c923d95945
esodell1 <esodell1@gmail.com>
2016-02-03 12:40:44 -0800

    Added persistent storage class.

--- src/Project/Control.java
+++ src/Project/Control.java
@@ -1,10 +1,39 @@
 package Project;
 
+import java.util.Collection;
+
 public class Control {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// Instance fields
+	private Collection<Job> jobs;
+	private Collection<Park> parks;
+	private Collection<User> users;
+	
+	
+	// Lists all jobs as a link to the Collection
+	protected Collection<Job> listJobs() {
+		return jobs;
 	}
-
+	
+	// Returns the number of Jobs in the system
+	protected int jobCount() {
+		return jobs.size();
+	}
+	
+	// Lists the users based on their role
+	protected Collection<User> listUsersByType() {
+		//TODO Limit search parameters
+		return users;
+	}
+	
+	
+	// Public constructor
+	public Control() {
+		super();
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+	}
 }
