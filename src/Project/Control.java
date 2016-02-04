af79034472761f9ef1438130a4414563a8247a85
esodell1 <esodell1@gmail.com>
2016-02-04 11:27:40 -0800

    Added backend.

--- src/Project/Control.java
+++ src/Project/Control.java
@@ -1,16 +1,18 @@
 package Project;
 
-import java.util.Collection;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Control {
 	// Instance fields
-	private Collection<Job> jobs;
-	private Collection<Park> parks;
-	private Collection<User> users;
+	private List<Job> jobs;
+	private List<Park> parks;
+	private List<User> users;
+	private Persistent db;
 	
 	
 	// Lists all jobs as a link to the Collection
-	protected Collection<Job> listJobs() {
+	protected List<Job> listJobs() {
 		return jobs;
 	}
 	
@@ -20,7 +22,7 @@ public class Control {
 	}
 	
 	// Lists the users based on their role
-	protected Collection<User> listUsersByType() {
+	protected List<User> listUsersByType() {
 		//TODO Limit search parameters
 		return users;
 	}
@@ -28,12 +30,50 @@ public class Control {
 	
 	// Public constructor
 	public Control() {
-		super();
+		jobs = new ArrayList<Job>();
+		parks = new ArrayList<Park>();
+		users = new ArrayList<User>();
+		db = new Persistent();
 	}
 	
-	
+	public void setupTestArrays() {
+		// 2 jobs
+		jobs.add(new Job());
+		jobs.add(new Job());
+		// 3 parks
+		parks.add(new Park());
+		parks.add(new Park());
+		parks.add(new Park());
+		// 6 Users
+		users.add(new Staff());
+		users.add(new Staff());
+		users.add(new Staff());
+		users.add(new Staff());
+		users.add(new Staff());
+		users.add(new Staff());
+		
+		System.out.println("Saving test arrays...");
+		db.saveData(jobs, parks, users);
+		
+		jobs = null;
+		parks = null;
+		users = null;
+				
+		System.out.println("Save complete. Loading data...");
+		db.loadData();
+		jobs = db.getJobs();
+		parks = db.getParks();
+		users = db.getUsers();
+		System.out.println("Load complete.");
+		System.out.println("Jobs arraylist size: " + jobs.size());
+		System.out.println("Parks arraylist size: " + parks.size());
+		System.out.println("Users arraylist size: " + users.size());
+		
+	}
 	
 	public static void main(String[] args) {
+		Control ctrl = new Control();
+		ctrl.setupTestArrays();
 		
 	}
 }
