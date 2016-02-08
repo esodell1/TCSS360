1c3c8ac1b38a9678ceffaf3442d5ee74ab8082cb
Edgardo Gutierrez Jr. <EdgardoGutierrezJr@Gmail.com>
2016-02-08 15:20:48 -0800

    Tested UI

--- src/Project/UITesting.java
+++ src/Project/UITesting.java
@@ -2,14 +2,15 @@ package Project;
 
 public class UITesting {
 
-  // MEANT FOR UI TESTING
-  public static void main(String[] theArgs) {
-    UserInterface ui = new UserInterface();
-    String[] content = {"xxxxxxxxxxx", "yyyyyyyyyy", "zzzzzzzzzz", "xxxxxxxxxxx", "yyyyyyyyyy", "zzzzzzzzzz", "xxxxxxxxxxx", "yyyyyyyyyy", "zzzzzzzzzz"};
+	// MEANT FOR UI TESTING
+	public static void main(String[] theArgs) {
+		String[] options = {"option1", "option2", "option3", "option4", "option5"};
+		String[] items = {"list item 1", "list item 2", "list item 3", "list item 4"};
+		UserInterface ui = new UserInterface();
+		System.out.println("User input: " + ui.detailsString("Title: detailsString", "Some details"));
+		System.out.println("User input: " + ui.optionsInt("Title: optionsInt", options));
+		System.out.println("User input: " + ui.listInt("Title: listInt", items, options));
+		System.out.println("User input: " + ui.detailsInt("Title: detailsInt", "Some details", options));
 
-    ui.setUser("BARACK", "OBAMA", "STAFF MEMBER");
-    int lol = ui.basicNavigation("Menu", "uno", "dos", "tres", "quatro?");
-    lol = ui.basicView("View", "uno", "dos", "tres", "quatro?", content);
-    System.out.println("User input was: " + lol);
-  }
+	}
 }
