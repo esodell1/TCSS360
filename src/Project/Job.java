57f6634e5f81967101a014c2de16dbf9194e18e1
Tyler Braden <tbraden@uw.edu>
2016-02-07 23:30:38 -0800

    general models, we need to get together and talk about what kind of constructor we want for the models.  I could make one for ever situation, but that seems kind of tedious.
    No unit testing is being done on this code yet.

--- src/Project/Job.java
+++ src/Project/Job.java
@@ -1,9 +1,74 @@
 package Project;
 
 import java.io.Serializable;
+import java.util.List;
 
 public class Job implements Serializable {
 	private static final long serialVersionUID = 6239276355031273785L;
+	
+	private String name;
+	private Park park;
+	private String date;
+	private String discription;
+	private List<Volunteer> enrolledVolunteers;
+	
+	public Job(String name, Park park, String date, String discription, List<Volunteer> volunteerList) {
+		this.name = name;
+		this.park = park;
+		this.date = date;
+		this.discription = discription;
+		this.enrolledVolunteers = volunteerList;
+	}
+	
+	public void addVolunteer(Volunteer volunteer) {
+		this.enrolledVolunteers.add(volunteer);
+	}
+	
+	public void removeVolunteer(String name) {
+		int index = this.enrolledVolunteers.indexOf(name);
+		this.enrolledVolunteers.remove(index);
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Park getPark() {
+		return park;
+	}
+
+	public void setPark(Park park) {
+		this.park = park;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public String getDiscription() {
+		return discription;
+	}
+
+	public void setDiscription(String discription) {
+		this.discription = discription;
+	}
+
+	public List<Volunteer> getenrolledVolunteers() {
+		return enrolledVolunteers;
+	}
+
+	public void setenrolledVolunteers(List<Volunteer> volunteerList) {
+		this.enrolledVolunteers = volunteerList;
+	}
+	
 
 	
 }
--- src/Project/Park.java
+++ src/Project/Park.java
@@ -3,7 +3,35 @@ package Project;
 import java.io.Serializable;
 
 public class Park implements Serializable {
+	
 	private static final long serialVersionUID = -5896375771655268671L;
+	
+	private String location;
+	private String manager;
+	
+	public Park(String location, String manager) {
+		
+		this.location = location;
+		this.manager = manager;
+				
+	}
 
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getManager() {
+		return manager;
+	}
+
+	public void setManager(String manager) {
+		this.manager = manager;
+	}
+	
+	
 	
 }
