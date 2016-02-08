1d9bf876131c228a37f15a2d650dd2fc3fc5d2c3
esodell1 <esodell1@gmail.com>
2016-02-08 10:00:09 -0800

    Removed temp Control testing code

--- src/Project/Control.java
+++ src/Project/Control.java
@@ -35,45 +35,4 @@ public class Control {
 		users = new ArrayList<User>();
 		db = new Persistent();
 	}
-	
-	public void setupTestArrays() {
-		// 2 jobs
-		jobs.add(new Job());
-		jobs.add(new Job());
-		// 3 parks
-		parks.add(new Park());
-		parks.add(new Park());
-		parks.add(new Park());
-		// 6 Users
-		users.add(new Staff());
-		users.add(new Staff());
-		users.add(new Staff());
-		users.add(new Staff());
-		users.add(new Staff());
-		users.add(new Staff());
-		
-		System.out.println("Saving test arrays...");
-		db.saveData(jobs, parks, users);
-		
-		jobs = null;
-		parks = null;
-		users = null;
-				
-		System.out.println("Save complete. Loading data...");
-		db.loadData();
-		jobs = db.getJobs();
-		parks = db.getParks();
-		users = db.getUsers();
-		System.out.println("Load complete.");
-		System.out.println("Jobs arraylist size: " + jobs.size());
-		System.out.println("Parks arraylist size: " + parks.size());
-		System.out.println("Users arraylist size: " + users.size());
-		
-	}
-	
-	public static void main(String[] args) {
-		Control ctrl = new Control();
-		ctrl.setupTestArrays();
-		
-	}
 }
