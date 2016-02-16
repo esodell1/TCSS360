3f8135d56879ae003d1cb7e8545ebe190dde10fd
Edgardo Gutierrez Jr. <EdgardoGutierrezJr@Gmail.com>
2016-02-15 16:20:27 -0800

    UI Update

--- src/Project/UserInterface.java
+++ src/Project/UserInterface.java
@@ -9,7 +9,7 @@ public class UserInterface {
 	private static final int DEFAULT_WIDTH = 80;
 	private static final int DEFAULT_HEIGHT = 24;
 
-	private static final String CURSOR = "[MENU-NUMBER]: ";
+	private static final String CURSOR = "> ";
 
 	public Scanner myScanner;
 	public String myFirstName;
@@ -46,16 +46,29 @@ public class UserInterface {
 			System.out.print("_");
 		}
 		System.out.println("\n");
+		System.out.print(CURSOR);
 	}
 
-	private void printUserInfo() {
-		System.out.println("\n    " + myUserType + ": " + myFirstName + " " + myLastName + "\n");
+	private String getUserInfo() {
+		return myUserType + ": " + myFirstName + " " + myLastName;
+	}
+	
+	private void printCentered(String theString) {
+		int stringLength;
+		
+		if (theString.length() % 2 == 1) stringLength = theString.length() + 1;
+		else stringLength = theString.length();
+		
+		int spaces = (DEFAULT_WIDTH - stringLength) / 2;
+		for (int i = 0; i < spaces; i++) {
+			System.out.print(" ");
+		}
+		System.out.println(theString);
 	}
 
 	private int getInt(int theLowerBound, int theUpperBound) {
 		myScanner = new Scanner(System.in);
 		int input = -100;
-		System.out.print(CURSOR);
 		try {
 			input = myScanner.nextInt();
 			if (!(input >= theLowerBound && input <= theUpperBound)) {
@@ -70,13 +83,19 @@ public class UserInterface {
 	public int optionsInt(String title, List<?> options) {
 		int input = -100;
 		do {
+			
+			printTopBorder(DEFAULT_WIDTH);
+			
+			printCentered(getUserInfo());
+			
 			// Title
-			System.out.println("Title: " + title);
+			printCentered(title);
 			
 			// Options
 			for (int i = 0; i < options.size(); i++) {
 				System.out.println((i + 1) + ".\t" + options.get(i));
 			}
+			printBottomBorder(DEFAULT_WIDTH);
 			input = getInt(1, options.size());
 		} while (input == -100);
 		return input;
@@ -85,19 +104,29 @@ public class UserInterface {
 	public String detailsString(String title, String details) {
 		myScanner = new Scanner(System.in);
 		
+		printTopBorder(DEFAULT_WIDTH);
+		
+		printCentered(getUserInfo());
+		
 		// Title
-		System.out.println("Title: " + title);
+		printCentered(title);
 		
 		// Details
 		System.out.println(details);
+		printBottomBorder(DEFAULT_WIDTH);
 		return myScanner.nextLine();
 	}
 
 	public int listInt(String title, List<String> items, List<String> options) {
 		int input = -100;
 		do {
+			
+			printTopBorder(DEFAULT_WIDTH);
+			
+			printCentered(getUserInfo());
+			
 			// Title
-			System.out.println("Title: " + title);
+			printCentered(title);
 			
 			// List
 			for (int i = 0; i < items.size(); i++) {
@@ -107,6 +136,7 @@ public class UserInterface {
 			for (int i = 0; i < options.size(); i++) {
 				System.out.println((i + 1) + ".\t" + options.get(i));
 			}
+			printBottomBorder(DEFAULT_WIDTH);
 			input = getInt(1, options.size());
 		} while (input == -100);
 		return input;
@@ -115,8 +145,13 @@ public class UserInterface {
 	public int detailsInt(String title, String details, List<String> options) {
 		int input = -100;
 		do {
+			
+			printTopBorder(DEFAULT_WIDTH);
+			
+			printCentered(getUserInfo());
+			
 			// Title
-			System.out.println("Title: " + title);
+			printCentered(title);
 			
 			// Details
 			System.out.println(details);
@@ -125,47 +160,11 @@ public class UserInterface {
 			for (int i = 0; i < options.size(); i++) {
 				System.out.println((i + 1) + ".\t" + options.get(i));
 			}
+			
+			printBottomBorder(DEFAULT_WIDTH);
+			
 			input = getInt(1, options.size());
 		} while (input == -100);
 		return input;
 	}
-
-//	public int basicView(String title, String item1, String item2, String item3, String item4, String[] theContent) {
-//		int spaceCount = (DEFAULT_WIDTH - 30 - title.length()) / 2;
-//		int input = -100;
-//		while (input == -100) {
-//			// Screen header
-//			printTopBorder(DEFAULT_WIDTH);
-//			printUserInfo();
-//
-//			// Top content bar
-//			System.out.print("               ");
-//			for (int i = 0; i < spaceCount; i++)
-//				System.out.print("_");
-//			System.out.print(title);
-//			for (int i = 0; i < spaceCount; i++)
-//				System.out.print("_");
-//			System.out.println();
-//			System.out.println();
-//
-//			// Content
-//			for (int i = 0; i < theContent.length; i++)
-//				System.out.println("\t\t" + theContent[i]);
-//			System.out.println("\n\t\t1.\t" + item1);
-//			System.out.println("\t\t2.\t" + item2);
-//			System.out.println("\t\t3.\t" + item3);
-//			System.out.print("\t\t4.\t" + item4 + "\n\t\t");
-//
-//			// Bottom content bar
-//			for (int i = 0; i < DEFAULT_WIDTH - 30; i++)
-//				System.out.print("_");
-//			System.out.println();
-//
-//			// Screen footer
-//			printBottomBorder(DEFAULT_WIDTH);
-//			input = getInt(1, 4);
-//		}
-//		return input;
-//	}
-
 }
