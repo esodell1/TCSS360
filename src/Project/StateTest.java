27cafee7bb9ad9d3f654c1f6f077e8842568f6b1
Edgardo Gutierrez Jr. <EdgardoGutierrezJr@Gmail.com>
2016-03-05 15:00:17 -0800

    New tests for states, need to finish implementing

--- src/Project/StateTest.java
+++ src/Project/StateTest.java
@@ -13,7 +13,7 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * @author Eric
+ * @author Eric & Elijah
  *
  */
 public class StateTest {
@@ -29,10 +29,8 @@ public class StateTest {
 	 */
 	@Before
 	public void setUp() throws Exception {
-		currentState = State.LOGIN;
 		ctrl = new Control();
 		ui = new UserInterface();
-		
 		ui.input = this.input;
 		ui.output = System.out;
 	}
@@ -45,18 +43,118 @@ public class StateTest {
 	 * Test method for {@link Project.State#nextState(Project.UserInterface, Project.Control)}.
 	 */
 	@Test
-	public void testLogin() {
+	public void test() {
+		// fullTest is a compilation of all tests
+		boolean fullTest = false;
+		
+		fullTest = (testLogin() && testMain() && testViewAllJobs()
+				&& testViewJob() && testDeleteJob() && testCreateJob()
+				&& testCreateJob2() && testCreateJob3() && testCreateJob4()
+				&& testCreateJob5() && testCreateJob6() && testConfirmJob()
+				&& testSearchLastName() && testJobSignup() && testViewJobVol()
+				&& testLogout() && testMyJobs() && testEditJobDetails() 
+				&& testErrorMsg() && testSuccessMsg());
+		assertTrue(fullTest);
+	}
+	
+	// Private login test
+	private boolean testLogin() {
+		// Testing a valid user, should result in the progression to main menu.
+		currentState = State.LOGIN;
 		input = new ByteArrayInputStream("manager@uw.edu".getBytes());
 		System.setIn(input);
 		next();
-		assertEquals(currentState, State.MAIN);
-		currentState = State.LOGIN;
+		boolean test1 = currentState == State.MAIN;
 		
+		// Testing a invalid user, should result in login state again.
+		currentState = State.LOGIN;
 		input = new ByteArrayInputStream("invalid@uw.edu".getBytes());
 		System.setIn(input);
 		next();
-		assertEquals(currentState, State.LOGIN);
+		boolean test2 = currentState == State.LOGIN;
 		
+		// Returns the compilation of all tests
+		return test1 && test2;
+	}
+	
+	private boolean testMain() {
+		return true;
+	}
+	
+	private boolean testViewAllJobs() {
+		return true;
+	}
+	
+	private boolean testViewJob() {
+		return true;
+	}
+	
+	private boolean testDeleteJob() {
+		return true;
+	}
+	
+	private boolean testCreateJob() {
+		return true;
+	}
+	
+	private boolean testCreateJob1() {
+		return true;
+	}
+	
+	private boolean testCreateJob2() {
+		return true;
+	}
+	
+	private boolean testCreateJob3() {
+		return true;
+	}
+	
+	private boolean testCreateJob4() {
+		return true;
+	}
+	
+	private boolean testCreateJob5() {
+		return true;
+	}
+	
+	private boolean testCreateJob6() {
+		return true;
+	}
+	
+	private boolean testConfirmJob() {
+		return true;
+	}
+	
+	private boolean testSearchLastName() {
+		return true;
+	}
+	
+	private boolean testJobSignup() {
+		return true;
+	}
+	
+	private boolean testViewJobVol() {
+		return true;
+	}
+	
+	private boolean testLogout() {
+		return true;
+	}
+	
+	private boolean testMyJobs() {
+		return true;
+	}
+	
+	private boolean testEditJobDetails() {
+		return true;
+	}
+	
+	private boolean testErrorMsg() {
+		return true;
+	}
+	
+	private boolean testSuccessMsg() {
+		return true;
 	}
 
 }
