b7751c3ff9846cc9c2a39a6b9233ddf8bc393769
Eric Odell <esodell1@gmail.com>
2016-03-06 12:08:48 -0800

    Added more state tests

--- src/Project/StateTest.java
+++ src/Project/StateTest.java
@@ -8,6 +8,9 @@ import static org.junit.Assert.*;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -146,13 +149,36 @@ public class StateTest {
 	
 	@Test
 	public void testViewAllJobs() {
+		currentState = State.LOGIN;
+		inputHelper("manager@uw.edu");		// Login as manager
+		next();	
+		assertEquals(currentState, State.MAIN);
 		
-		assertTrue(true);
+		currentState = State.VIEW_ALL_JOBS;	// View jobs state
+		
+		List<Job> jobs = ctrl.jobs;			// Create job name list directly from Control class
+		List<String> jobNames = new ArrayList<String>();
+		for (int i = 0; i < jobs.size(); i++) {
+			if (jobs.get(i).getStartDate().after(Calendar.getInstance())) {
+				jobNames.add(jobs.get(i).getName());
+			}
+		}
+		assertEquals(jobNames, ctrl.getAllJobs());	// Ensure job lists are equivalent
 	}
 	
 	@Test
 	public void testViewJob() {
-		assertTrue(true);
+		currentState = State.LOGIN;
+		inputHelper("manager@uw.edu");		// Login as manager
+		next();	
+		assertEquals(currentState, State.MAIN);
+		
+		currentState = State.VIEW_ALL_JOBS;	// View jobs state
+		if (ctrl.getAllJobs().size() <= 0) fail("Need at least one Job to test View Job state.");
+		inputHelper("1");
+		next();
+		
+		assertEquals(currentState, State.VIEW_JOB);
 	}
 	
 	@Test
@@ -162,22 +188,96 @@ public class StateTest {
 	
 	@Test
 	public void testCreateJob() {
-		assertTrue(true);
+		currentState = State.LOGIN;
+		inputHelper("manager@uw.edu");		// Login as manager
+		next();	
+		assertEquals(currentState, State.MAIN);
+		
+		currentState = State.CREATE_JOB;	// Create job state
+		String testName = "This is a test job created by JUnit.";
+		inputHelper(testName);
+		next();
+		
+		assertEquals(currentState, State.CREATE_JOB_2); 	// Verify name was accepted.
+		assertEquals(ctrl.getCurrentJob().getName(), testName);
+		
+		// No further input restrictions
 	}
 	
 	@Test
 	public void testCreateJob2() {
-		assertTrue(true);
+		testCreateJob();
+		
+		String testDescription = "Test Job Description - No input restrictions here, this is sufficient.";
+		inputHelper(testDescription);
+		next();
+		
+		assertEquals(currentState, State.CREATE_JOB_3);
+		assertEquals(ctrl.getCurrentJob().getDescription(), testDescription);
 	}
 	
 	@Test
 	public void testCreateJob3() {
-		assertTrue(true);
+		testCreateJob2();
+		
+		Administrator user;
+		try {
+			user = (Administrator) ctrl.getCurrentUser();
+			List<String> parks = user.getParkNames();
+			if (parks.size() <= 0) {
+				fail("Must have at least one park associated with user to make a job.");
+			} else {
+				inputHelper("1");		// UI only accepts integers in range of selection.
+				next();
+				
+				assertEquals(currentState, State.CREATE_JOB_4);		// check correct state transition
+				assertNotNull(ctrl.getCurrentJob().getPark());
+			}
+		} 
+		catch(Exception e) {
+			fail(e.toString());
+		}
+		
+				
+		
 	}
 	
 	@Test
 	public void testCreateJob4() {
-		assertTrue(true);
+		testCreateJob3();
+		assertEquals(currentState, State.CREATE_JOB_4);
+		
+		// ------- Job start time and date test --------
+		
+		// Test invalid inputs:
+		inputHelper(" ");
+		next();
+		assertEquals(currentState, State.CREATE_JOB_4);
+		
+		inputHelper("this is an invalid start time and/or date");
+		next();
+		assertEquals(currentState, State.CREATE_JOB_4);
+		
+		// Test past date:
+		inputHelper("3/1/2016 14:00");
+		next();
+		assertEquals(currentState, State.ERROR_MSG);
+		
+		// Test job more than 90 days in advance:
+		testCreateJob3();
+		int month = Calendar.getInstance().get(Calendar.MONTH) + 4;
+		int day = Calendar.getInstance().get(Calendar.DATE) + 1;
+		inputHelper(month + "/" + day + "/2016 14:00");
+		next();
+		assertEquals(currentState, State.ERROR_MSG);
+		
+		// Test correct job date and time setting:
+		testCreateJob3();
+		month = Calendar.getInstance().get(Calendar.MONTH) + 1;
+		day = Calendar.getInstance().get(Calendar.DATE);
+		inputHelper(month + "/" + day + "/2016 14:00");
+		next();
+		assertEquals(currentState, State.CREATE_JOB_5);
 	}
 	
 	@Test
