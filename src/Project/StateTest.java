8f08d5b55df9800d38bc79ccc7d91c15a8171442
Edgardo Gutierrez Jr. <EdgardoGutierrezJr@Gmail.com>
2016-03-05 16:19:14 -0800

    Changed all methods to unit tests

--- src/Project/StateTest.java
+++ src/Project/StateTest.java
@@ -39,122 +39,151 @@ public class StateTest {
 		currentState = currentState.nextState(ui, ctrl);
 	}
 
-	/**
-	 * Test method for {@link Project.State#nextState(Project.UserInterface, Project.Control)}.
-	 */
-	@Test
-	public void test() {
-		// fullTest is a compilation of all tests
-		boolean fullTest = false;
-		
-		fullTest = (testLogin() && testMain() && testViewAllJobs()
-				&& testViewJob() && testDeleteJob() && testCreateJob()
-				&& testCreateJob2() && testCreateJob3() && testCreateJob4()
-				&& testCreateJob5() && testCreateJob6() && testConfirmJob()
-				&& testSearchLastName() && testJobSignup() && testViewJobVol()
-				&& testLogout() && testMyJobs() && testEditJobDetails() 
-				&& testErrorMsg() && testSuccessMsg());
-		assertTrue(fullTest);
-	}
+//	/**
+//	 * Test method for {@link Project.State#nextState(Project.UserInterface, Project.Control)}.
+//	 */
+//	@Test
+//	public void test() {
+//		// fullTest is a compilation of all tests
+//		boolean fullTest = false;
+//		
+//		fullTest = (testLogin() && testMain() && testViewAllJobs()
+//				&& testViewJob() && testDeleteJob() && testCreateJob()
+//				&& testCreateJob2() && testCreateJob3() && testCreateJob4()
+//				&& testCreateJob5() && testCreateJob6() && testConfirmJob()
+//				&& testSearchLastName() && testJobSignup() && testViewJobVol()
+//				&& testLogout() && testMyJobs() && testEditJobDetails() 
+//				&& testErrorMsg() && testSuccessMsg());
+//		assertTrue(fullTest);
+//	}
 	
-	// Private login test
-	private boolean testLogin() {
+	@Test
+	private void testLoginValidUser() {
 		// Testing a valid user, should result in the progression to main menu.
 		currentState = State.LOGIN;
-		input = new ByteArrayInputStream("manager@uw.edu".getBytes());
-		System.setIn(input);
+		inputHelper("manager@uw.edu");
 		next();
-		boolean test1 = currentState == State.MAIN;
-		
+		assertTrue(currentState == State.MAIN);
+	}	
+	
+	@Test
+	private void testLoginInvalidUser() {
 		// Testing a invalid user, should result in login state again.
 		currentState = State.LOGIN;
 		input = new ByteArrayInputStream("invalid@uw.edu".getBytes());
 		System.setIn(input);
-		next();
-		boolean test2 = currentState == State.LOGIN;
-		
-		// Returns the compilation of all tests
-		return test1 && test2;
+		next();	
+		assertTrue(currentState == State.LOGIN);
 	}
 	
+	@Test
 	private boolean testMain() {
+		// Testing menu as staff.
+		currentState = State.LOGIN;
+
+		
+		// Testing menu as manager.
+
+		// Testing menu as volunteer.
+
+		
+		
 		return true;
 	}
 	
+	@Test
 	private boolean testViewAllJobs() {
 		return true;
 	}
 	
+	@Test
 	private boolean testViewJob() {
 		return true;
 	}
 	
+	@Test
 	private boolean testDeleteJob() {
 		return true;
 	}
 	
+	@Test
 	private boolean testCreateJob() {
 		return true;
 	}
 	
-	private boolean testCreateJob1() {
-		return true;
-	}
-	
+	@Test
 	private boolean testCreateJob2() {
 		return true;
 	}
 	
+	@Test
 	private boolean testCreateJob3() {
 		return true;
 	}
 	
+	@Test
 	private boolean testCreateJob4() {
 		return true;
 	}
 	
+	@Test
 	private boolean testCreateJob5() {
 		return true;
 	}
 	
+	@Test
 	private boolean testCreateJob6() {
 		return true;
 	}
 	
+	@Test
 	private boolean testConfirmJob() {
 		return true;
 	}
 	
+	@Test
 	private boolean testSearchLastName() {
 		return true;
 	}
 	
+	@Test
 	private boolean testJobSignup() {
 		return true;
 	}
 	
+	@Test
 	private boolean testViewJobVol() {
 		return true;
 	}
 	
+	@Test
 	private boolean testLogout() {
 		return true;
 	}
 	
+	@Test
 	private boolean testMyJobs() {
 		return true;
 	}
 	
+	@Test
 	private boolean testEditJobDetails() {
 		return true;
 	}
 	
+	@Test
 	private boolean testErrorMsg() {
 		return true;
 	}
 	
+	@Test
 	private boolean testSuccessMsg() {
 		return true;
+	}
+	
+	@Test
+	private void inputHelper(String theInput) {
+		System.setIn(new ByteArrayInputStream(theInput.getBytes()));
 	}
 
 }
