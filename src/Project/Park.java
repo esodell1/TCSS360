bf4e353070ee388afc9be8e3b7cf8c533fefdbf5
Travis <tbs95@uw.edu>
2016-03-07 02:01:30 -0800

    Added comments to Park class.

--- src/Project/Park.java
+++ src/Project/Park.java
@@ -27,38 +27,83 @@ public class Park implements Serializable {
 		this.myJobs = new ArrayList<Job>();
 	}
 
+	/**
+	 * Returns location of Park.
+	 * 
+	 * @return String with Park location.
+	 */
 	public String getLocation() {
 		return location;
 	}
 
+	/**
+	 * Sets location of Park.
+	 * 
+	 * @param location String of Park location.
+	 */
 	public void setLocation(String location) {
 		this.location = location;
 	}
 
+	/**
+	 * Gets the Park manager.
+	 * 
+	 * @return User whom manages the Park.
+	 */
 	public User getManager() {
 		return manager;
 	}
 
+	/**
+	 * Sets the Park manager.
+	 * 
+	 * @param manager User who will manage the Park.
+	 */
 	public void setManager(User manager) {
 		this.manager = manager;
 	}
 	
+	/**
+	 * Gets Park Name.
+	 * 
+	 * @return String of park name.
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/**
+	 * Sets the name of the Park.
+	 * 
+	 * @param name New name of park.
+	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
+	/**
+	 * Gets all Jobs associated with Park.
+	 * 
+	 * @return List of Jobs associated with this Park
+	 */
 	public List<Job> getMyJobs() {
 		return myJobs;
 	}
-
+	
+	/**
+	 * Adds a new Job to this Park.
+	 * 
+	 * @param newJob Job to be added to Park.
+	 */
 	public void addJob(Job newJob) {
 		this.myJobs.add(newJob);
 	}
-	/**outputs all information regarding the park.*/
+	
+	/**
+	 * Outputs Park name, location, and manager
+	 * 
+	 * @return String representation of Park name, location, and manager.
+	 */
 	public String toString() {
 		StringBuilder park_info = new StringBuilder();
 		park_info.append("\n\tPark: \t\t");
