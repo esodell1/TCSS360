ae3508a9f13c15014d9db0a40dc38c3874239b4c
tbraden <tbraden@IT30154788.insttech.washington.edu>
2016-03-07 12:36:52 -0800

    added all comment for our Job testing class.

--- src/Project/JobTest.java
+++ src/Project/JobTest.java
@@ -22,6 +22,11 @@ public class JobTest {
 	User staff, manager, volunteer;
 	Calendar cal, cal2, calTest;
 
+	/**
+	 * This method sets up values needed for testing our Job class.
+	 * 
+	 * @throws Exception
+	 */
 	@Before
 	public void setUp() throws Exception {
 		staff = new Staff("Eric", "Odell", "staff@uw.edu", WorkLoad.HIGH);
@@ -39,13 +44,31 @@ public class JobTest {
 		initialPark.addJob(initialJob);
 	}
 
+	/**
+	 * This method tests if the add volunteer method is successful.
+	 * 
+	 * @assertEquals 1st - we test to make sure that the list of volunteers exists
+	 * by checking to see if the volunteerList size is currently 0
+	 * 
+	 * @assertEquals 2nd - we test to make sure that the list has 1 volunteer added,
+	 * the size should go from 0 to 1.
+	 */
 	@Test
 	public void testAddVolunteer() {
 		assertEquals(initialJob.getEnrolledVolunteers().size(), 0);
 		initialJob.addVolunteer((Volunteer) volunteer, WorkLoad.MEDIUM);
 		assertEquals(initialJob.getEnrolledVolunteers().size(), 1);
 	}
-
+	
+	/**
+	 * This method tests if the remove volunteer method is successful.
+	 * 
+	 * @assertEquals 1st - we test to make sure that the list of volunteers exists
+	 * by checking to see if the volunteerList size is currently 1
+	 * 
+	 * @assertEquals 2nd - we test to make sure that the list has 1 volunteer removed,
+	 * the size should go from 1 to 0.
+	 */
 	@Test
 	public void testRemoveVolunteer() {
 		initialJob.addVolunteer((Volunteer) volunteer, WorkLoad.MEDIUM);
@@ -53,12 +76,27 @@ public class JobTest {
 		initialJob.removeVolunteer(volunteer);
 		assertEquals(initialJob.getEnrolledVolunteers().size(), 0);
 	}
-
+	
+	/**
+	 * This method tests if the start date is correct.
+	 * 
+	 * @assertEquals 1st - we test to make sure that the start date is equal to
+	 * the target start date.
+	 */
 	@Test
 	public void testGetStartDate() {
 		assertEquals(cal, initialJob.getStartDate());
 	}
 
+	/**
+	 * This method tests if the set date method works correctly.
+	 * 
+	 * @assertNotEquals - we test to make sure that the start date is  not equal to
+	 * the target start date.
+	 * 
+	 * @assertEquals - we test to make sure that the start date is equal to
+	 * the target start date after being set.
+	 */
 	@Test
 	public void testSetStartDate() {
 		assertNotEquals(calTest, initialJob.getStartDate());
@@ -67,12 +105,27 @@ public class JobTest {
 				calTest.get(Calendar.MINUTE));
 		assertEquals(calTest, initialJob.getStartDate());
 	}
-
+	
+	/**
+	 * This method tests if the end date is correct.
+	 * 
+	 * @assertEquals 1st - we test to make sure that the end date is equal to
+	 * the target end date.
+	 */
 	@Test
 	public void testGetEndDate() {
 		assertEquals(cal2, initialJob.getEndDate());
 	}
 
+	/**
+	 * This method tests if the set end date method works correctly.
+	 * 
+	 * @assertNotEquals - we test to make sure that the end date is not equal to
+	 * the target end date.
+	 * 
+	 * @assertEquals - we test to make sure that the end date is equal to
+	 * the target end date after being set.
+	 */
 	@Test
 	public void testSetEndDate() {
 		assertNotEquals(calTest, initialJob.getEndDate());
@@ -81,7 +134,16 @@ public class JobTest {
 				calTest.get(Calendar.MINUTE));
 		assertEquals(calTest, initialJob.getEndDate());
 	}
-
+	
+	/**
+	 * This method tests if get enrolled volunteer method is working.
+	 * 
+	 * @assertEquals 1st - we test to make sure that the list of volunteers exists
+	 * by checking to see if the volunteerList size is currently 0
+	 * 
+	 * @assertEquals 2nd - we test to make sure that the list has 1 volunteer added,
+	 * the size should go from 0 to 1.
+	 */
 	@Test
 	public void testGetEnrolledVolunteers() {
 		assertEquals(initialJob.getEnrolledVolunteers().size(), 0);
@@ -89,6 +151,13 @@ public class JobTest {
 		assertEquals(initialJob.getEnrolledVolunteers().size(), 1);
 	}
 
+	/**
+	 * This method tests object if the object is equal to this object.
+	 * 
+	 * @assertEquals - we test to make sure that the object is equal to
+	 * this object.  Also, Equals methods should have there own testing from the 
+	 * java.lang.Object library.
+	 */
 	@Test
 	public void testEqualsObject() {
 		Job testJob = new Job("Trash Pickup", initialPark, cal, cal2,
